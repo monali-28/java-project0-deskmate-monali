35bc5e72cd1d27bd890de96290eed34553c47680
monali-28 <monalikhairnar28@gmail.com>
2026-02-18 15:09:57 +0530

    Update AppConfig

--- src/main/java/com/deskmate/config/AppConfig.java
+++ src/main/java/com/deskmate/config/AppConfig.java
@@ -1,5 +1,33 @@
 package com.deskmate.config;
 
+import com.deskmate.controller.DeskController;
+import com.deskmate.dao.DeskDao;
+import com.deskmate.dao.JdbcDeskDao;
+import com.deskmate.services.DeskService;
+
 public class AppConfig {
+
+	    public DeskController deskController() {
+	        DeskDao deskDao = new JdbcDeskDao();
+	        DeskService deskService = new DeskService(deskDao);
+	        return new DeskController(deskService);
+	    }
+
+//	    public BookingController bookingController() {
+//	        DeskDao deskDao = new JdbcDeskDao();
+//	        BookingDao bookingDao = new JdbcBookingDao();
+//	        PaymentDao paymentDao = new JdbcPaymentDao();
+//	        BookingService bookingService = new BookingService(deskDao, bookingDao, paymentDao);
+//	        return new BookingController(bookingService);
+//	    }
+//
+//	    public ReportController reportController() {
+//	        ReportDao reportDao = new JdbcReportDao();
+//	        ReportService reportService = new ReportService(reportDao);
+//	        return new ReportController(reportService);
+//	    }
+//
+//
+
 
 }
